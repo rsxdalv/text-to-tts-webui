ef28518798e15a5e16bd27f9dcfcf1844d9965b0
Roberts Slisans <rsxdalv@gmail.com>
2025-06-23 00:59:27 +0300

    init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Kokoro-Based TTS Extension for [Oobabooga Text Generation WebUI](https://github.com/oobabooga/text-generation-webui)
+# [TTS WebUI](https://github.com/rsxdalv/tts-webui) Extension for [Oobabooga Text Generation WebUI](https://github.com/oobabooga/text-generation-webui)
 
 Enhance your text generation experience with the Kokoro TTS extension, seamlessly integrating with the Oobabooga Text Generation WebUI.
 
@@ -8,77 +8,16 @@ Enhance your text generation experience with the Kokoro TTS extension, seamlessl
 
 - **Model Weights:** The model weights are **not** covered by the MIT License. They are licensed under the [Apache 2.0 License](https://huggingface.co/hexgrad/Kokoro-82M) and will be directly downloaded from Hugging Face.
 
-## Features
-
-**Current Version:** Kokoro v1
-**Supported Languages:** English
-
-Kokoro TTS is limited to inputs up to **510 tokens**. *Note that Kokoro tokens differ from LLM tokens.* This extension allows you to generate longer audio outputs by splitting the input text into segments and concatenating the resulting audio.
-
-## Audio Play
-> [!NOTE] 
-> Currently, the Audio Play feature is not available in the main branch as it is still in development, but you can see [here](https://github.com/h43lb1t0/KokoroTtsTexGernerationWebui/blob/personas/Audio_Play.md) how to use it.
-
-Unique TTS voices are assigned to each speaker in dialogue to get a audio play like experience. 
-
-### Text Splitting Methods
-
-- **Split by Sentence:** Divides the text into chunks of complete sentences, each chunk containing fewer than or 510 tokens.
-    - This method may fail to split the text into sentences if the input text contains unusual punctuation or formatting. In such cases, the extension will fall back to the "Split by Word" method for this one text.
-- **Split by Word:** Divides the text into chunks of individual words, each chunk containing fewer than or 510 tokens.
-
-*I recommend using the "Split by Sentence" method to maintain context and ensure higher quality audio output.*
-
 ## Installation
 
-### Prerequisites
+Install [TTS WebUI](https://github.com/rsxdalv/tts-webui) and activate the OpenAI compatible TTS API under *Tools > OpenAI API*.
 
-Before installing the extension, ensure you have the following dependencies installed:
+Once TTS WebUI's OpenAI API is running, you can install this extension by following the instructions below:
 
-- **eSpeak:** Download from [eSpeak NG Releases](https://github.com/espeak-ng/espeak-ng/releases).
-- **FFmpeg:** Download from [FFmpeg Downloads](https://ffmpeg.org/download.html).
+1. Clone this repository into the `extensions` directory of your Oobabooga Text Generation WebUI installation.
+2. Install the required dependencies by running `pip install -r requirements.txt` in the extension's directory.
+3. Restart the Oobabooga Text Generation WebUI.
 
-### Python Dependencies
+## Acknowledgements
 
-Install the required Python packages using the appropriate script for your operating system.
-
-#### Windows
-
-1. Run the Windows setup script:
-    ```cmd
-    .\cmd_windows.bat
-    ```
-2. Install the Python dependencies:
-    ```cmd
-    pip install -r extensions\KokoroTtsTextGenerationWebUI\requirements.txt
-    ```
-
-#### Linux
-
-1. Run the Linux setup script:
-    ```bash
-    ./cmd_linux.sh
-    ```
-2. Install the Python dependencies:
-    ```bash
-    pip install -r extensions/KokoroTtsTextGenerationWebUI/requirements.txt
-    ```
-
-## Multiple GPU Support
-
-By default, the extension utilizes the first available GPU. To specify a different GPU, modify the `device` variable in `src/generate.py` to your desired GPU identifier.
-
-## Roadmap
-
-- [x] Implement the extension
-- [x] ~~Kokoro v0.19~~
-- [x] Kokoro v1
-- [x] Support for all operating systems
-- [x] Voice selection feature
-- [ ] Only switch to "Split by Word" for the failing text part, not the entire text
-- [ ] Support for other languages than English
-- [ ] Support for future versions of Kokoro
-
-## Contributing
-
-I welcome contributions to improve this project! If you'd like to contribute, please create a pull request or open an issue. Your improvements and suggestions are highly appreciated.
+This extension is built upon the [KokoroTtsTexGernerationWebui](https://github.com/h43lb1t0/KokoroTtsTexGernerationWebui) repository by [Tom Haelbich](https://github.com/h43lb1t0). License of his work is in the [LICENSE.derived](LICENSE.derived) file.
--- script.py
+++ script.py
@@ -1,71 +1,127 @@
 import pathlib
 import html
 import time
-from extensions.KokoroTtsTexGernerationWebui.src.generate import run, load_voice, set_plitting_type
-from extensions.KokoroTtsTexGernerationWebui.src.voices import VOICES
 import gradio as gr
 import time
 
-from modules import shared
+params = {
+    "display_name": "TTS WebUI",
+    "is_tab": True,
+}
 
-def input_modifier(string, state):
 
-    shared.processing_message = "*Is recording a voice message...*"
-    return string
+def history_modifier(history):
+    """
+    Modifies the chat history.
+    Only used in chat mode.
+    """
+    return history
+
+
+def custom_css():
+    return ""
+
+
+def custom_js():
+    return ""
+
+
+def setup():
+    pass
 
 
 def voice_update(voice):
     load_voice(voice)
-    return gr.Dropdown(choices=VOICES, value=voice, label="Voice", info="Select Voice", interactive=True)
+    return gr.Dropdown(
+        choices=VOICES,
+        value=voice,
+        label="Voice",
+        info="Select Voice",
+        interactive=True,
+    )
+
 
 def voice_preview():
-    run("This is a preview of the selected voice", preview=True)
-    audio_dir = pathlib.Path(__file__).parent / 'audio' / 'preview.wav'
-    audio_url = f'{audio_dir.as_posix()}?v=f{int(time.time())}'
+    msg_id = generate_audio("This is a preview of the selected voice")
+    audio_dir = pathlib.Path(__file__).parent / "audio" / f"{msg_id}.wav"
+    audio_url = f"{audio_dir.as_posix()}?v=f{int(time.time())}"
     return f'<audio controls><source src="file/{audio_url}" type="audio/mpeg"></audio>'
-   
+
 
 def ui():
     info_voice = """Select a Voice. \nThe default voice is a 50-50 mix of Bella & Sarah\nVoices starting with 'a' are American
      english, voices with 'b' are British english"""
     with gr.Accordion("Kokoro"):
-        voice = gr.Dropdown(choices=VOICES, value=VOICES[0], label="Voice", info=info_voice, interactive=True)
+        voice = gr.Dropdown(
+            choices=[
+                "af_heart",
+                "af_heart2",
+            ],
+            value="af_heart",
+            label="Voice",
+            info=info_voice,
+            interactive=True,
+        )
 
         preview = gr.Button("Voice preview", type="secondary")
 
         preview_output = gr.HTML()
 
-        info_splitting ="""Kokoro only supports 510 tokens. One method to split the text is by sentence (default), the otherway
+        info_splitting = """Kokoro only supports 510 tokens. One method to split the text is by sentence (default), the otherway
         is by word up to 510 tokens. """
-        spltting_method = gr.Radio(["Split by sentence", "Split by Word"], info=info_splitting, value="Split by sentence", label_lines=2, interactive=True)
-
+        spltting_method = gr.Radio(
+            ["Split by sentence", "Split by Word"],
+            info=info_splitting,
+            value="Split by sentence",
+            label_lines=2,
+            interactive=True,
+        )
 
     voice.change(voice_update, voice)
     preview.click(fn=voice_preview, outputs=preview_output)
 
-    spltting_method.change(set_plitting_type, spltting_method)
-
-    
+    # spltting_method.change(set_plitting_type, spltting_method)
 
 
+def generate_audio(text: str):
+    import requests
 
-    
+    response = requests.post(
+        "http://localhost:7778/v1/audio/speech",
+        json={
+            "model": "hexgrad/Kokoro-82M",
+            # "input": "Hello world with custom parameters.",
+            "input": text,
+            "voice": "af_heart",
+            "speed": 1.0,
+            "params": {
+                "pitch_up_key": "2",
+                "index_path": "CaitArcane/added_IVF65_Flat_nprobe_1_CaitArcane_v2",
+            },
+        },
+    )
+    audio = response.content
 
-def output_modifier(string, state):
+    msg_id = f"audio_{int(time.time())}.wav"
+    audio_path = pathlib.Path(__file__).parent / "audio" / f"{msg_id}.wav"
+    with open(audio_path, "wb") as f:
+        f.write(audio)
+    return msg_id
 
+
+def output_modifier(string, state, is_chat=False):
 
     # Escape the string for HTML safety
     string_for_tts = html.unescape(string)
-    string_for_tts = string_for_tts.replace('*', '')
-    string_for_tts = string_for_tts.replace('`', '')
+    string_for_tts = string_for_tts.replace("*", "")
+    string_for_tts = string_for_tts.replace("`", "")
 
- 
     # Run your custom logic to generate audio
-    msg_id = run(string_for_tts)
+    # msg_id = run(string_for_tts)
+    msg_id = generate_audio(string_for_tts)
 
     # Construct the correct path to the 'audio' directory
-    audio_dir = pathlib.Path(__file__).parent / 'audio' / f'{msg_id}.wav'
-
+    audio_dir = pathlib.Path(__file__).parent / "audio" / f"{msg_id}.wav"
 
     # Add the audio playback HTML to the output string
     string += f'<audio controls><source src="file/{audio_dir.as_posix()}" type="audio/mpeg"></audio>'
